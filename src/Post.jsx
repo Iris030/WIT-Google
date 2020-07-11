f00f6ec1537fc8f2f6e064fb89a5e16de2bdb614
Yingxin Guo <z5158262@student.unsw.edu.au>
2020-07-12 03:01:26 +1000

    posts. still need to be updated more

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,5 +1,84 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import { Link } from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 600,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+export default function RecipeReviewCard(props) {
+  const classes = useStyles();
+  
+  var userName;
+  //console.log(props.form.name);
+  if (props.form.name.can_public === false) {
+    userName = "Anonymous";
+  } else {
+    userName = props.form.name.value;
+  }
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {userName[0]}
+          </Avatar>
+        }
+        title={props.form.title}
+        subheader={props.form.Time}
+      />
+      <CardMedia
+        className={classes.media}
+        image={props.form.image}
+        //title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.form.Content}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <Link to={`/form`}>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </Link>
+      </CardActions>
+    </Card>
+    
+  );
+}
+
+/*
 class Post extends React.Component {
   render() {
     return <p>{this.props.form.name}</p>;
@@ -7,3 +86,4 @@ class Post extends React.Component {
 }
 
 export default Post;
+*/
--- src/PostContainer.jsx
+++ src/PostContainer.jsx
@@ -2,11 +2,97 @@ import React from "react";
 import Post from "./Post";
 
 class PostContainer extends React.Component {
-  render() {
-    const result = [{ id: 1, name: "hi" }];
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: []
+    };
+  }
+  componentDidMount() {
+    this.setState({loading: true})
+    fetch("http://localhost:8081/getFormsByType?type=${this.props.location.pathname}")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          tasks: data
+        })
+      })
+  }
+
+  render() {    
+    /*
+    const result = [{
+    id : 1,
+    name:{
+      value:"String",
+      can_public:false
+    },
+    Address:{
+    value:"String",
+    can_public:false
+    },
+    Contact_info:{
+      value:"String",
+      can_public: false
+    },
+    Pic:{
+      value:"String",
+      can_public: false
+    },
+    Time:"String",
+    Type:"String",
+    Content:"String",
+    Services:[],
+    Resolve:{
+      status:false,
+      helper:"String"
+    },
+
+    Replies:[
+      {
+    username:"name",
+        reply:"String"
+      },
+    ],
+    },
+    {
+      id : 2,
+      name:{
+        value:"String",
+        can_public:false
+      },
+      Address:{
+      value:"String",
+      can_public:false
+      },
+      Contact_info:{
+        value:"String",
+        can_public: false
+      },
+      Pic:{
+        value:"String",
+        can_public: false
+      },
+      Time:"String",
+      Type:"String",
+      Content:"String",
+      Services:[],
+      Resolve:{
+        status:false,
+        helper:"String"
+      },
+  
+      Replies:[
+        {
+      username:"name",
+          reply:"String"
+        },
+      ],
+      }
+    ];*/
     return (
       <div>
-        {result.map((r) => {
+        {this.state.tasks.map((r) => {
           return <Post form={r} />;
         })}
       </div>
