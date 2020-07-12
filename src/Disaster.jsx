24f76975652582213f07d27c4da75d1f872ef502
julieduan <z5158357@student.unsw.edu.au>
2020-07-12 10:04:43 +1000

    added fetch

--- src/Disaster.jsx
+++ src/Disaster.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -8,7 +8,6 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
-import { ReactComponent } from "*.svg";
 
 const useStyles = makeStyles({
   root: {
@@ -19,62 +18,51 @@ const useStyles = makeStyles({
   },
 });
 
-export default class Disaster extends ReactComponent {
-  constructor(props) {
-    this.state = { number: 0 };
-  }
+export default function MediaCard(props) {
+  const classes = useStyles();
+  const [count, setCount] = useState(0);
 
-  classes = useStyles();
-
-  componentDidMount() {
-    fetch(`https://localhost:8081//getNumberByType?type=${this.props.type}`)
+  useEffect(() => {
+    fetch(`https://localhost:8081//getNumberByType?type=${props.type}`)
       .then((res) => res.json())
       .then(
         (result) => {
-          this.setState({
-            number: result,
-          });
+          setCount(result);
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
+        (error) => {}
       );
-  }
-  render() {
-    return (
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={this.props.image}
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {this.props.type}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {this.props.description}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              There're {props.number} posts
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Link to={`/posts/${this.props.type}`}>
-            <Button size="small" color="primary">
-              More
-            </Button>
-          </Link>
-        </CardActions>
-      </Card>
-    );
-  }
+  });
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.type}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.description}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            There're {count} posts
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Link to={`/posts/${props.type}`}>
+          <Button size="small" color="primary">
+            More
+          </Button>
+        </Link>
+      </CardActions>
+    </Card>
+  );
 }
